d87206c66c8fd7254995f18bf85ef26febba0f47
ll <48448919+llsccm@users.noreply.github.com>
2024-04-06 01:43:23 +0800

    feat: 使用本地配置文件

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import fs from 'fs'
 import Config from './src/parser.js'
-import download from './src/utils/download.js'
+// import download from './src/utils/download.js'
 
-const configUrl = 'https://web.sanguosha.com/220/h5_2/res/config/Config_w.sgs'
+// const configUrl = `${/** link */}/res/config/Config_w.sgs`
 const fileName = './dist/Config_w.sgs'
 
 const configer = new Config()
-await download(configUrl, fileName)
-const data = await fs.promises.readFile(fileName)
-configer.ParsePreloadConfig(data)
+// await download(configUrl, fileName)
+// const data = await fs.promises.readFile(fileName)
+// configer.ParsePreloadConfig(data)
 
 // const data = await fs.promises.readFile(fileName)
-// fs.readFile(fileName, function (err, data) {
-//   if (err) throw err
-//   configer.ParsePreloadConfig(data)
-// })
+fs.readFile(fileName, function (err, data) {
+  if (err) throw err
+  configer.ParsePreloadConfig(data)
+})
